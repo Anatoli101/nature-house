9175ea741b358209f259ffad3c493ac590264a77
Anatolii <tolikbatushan@gmail.com>
2019-01-31 16:08:45 +0100

    Update 2
    Adding animation, minified css code, more responsive features,adding backup version

--- js/script.js
+++ js/script.js
@@ -1,21 +1,21 @@
-// $("block_button").onclick(function() {
+// $("block-button").onclick(function() {
 //   $(this).addClass('.active');
 
 //   });
 
 
 $(document).ready(function () {
-    $(".block_button").click(function () {
+    $(".block-button").click(function () {
         $(".active").removeClass("active");
         $(this).addClass("active");
     });
 
-    $(".search_button").click(function () {
+    $(".search-button").click(function () {
         $(this).addClass("search-active");
     });
 
 
-    $(".search_button").click(function () {
+    $(".search-button").click(function () {
         if ($(".search").hasClass("search-area")) {
             $(".search").removeClass("search-area");
             $(".search").slideToggle('slow');
@@ -83,10 +83,119 @@ $(".sex-icon").click(function(){
 
 $(".mobile-menu").click(function(){
     $(".burger-menu").slideToggle('slow');
-
-  
 })
 
 $(".menu-button").click(function(){
     $(".burger-menu").slideToggle('300');
 })
+
+
+// SHOW MORE BUTTON
+
+var content = $(".lose-weight-text");
+var button = $(".show-more-button");
+
+button.click(function(){
+    if(content.hasClass("open")){
+		content.removeClass('open');
+		button.text("Rozwij");
+	} else {
+        content.addClass('open');
+        button.text("Zwij");
+	}
+});
+
+ // FOR SELECTRIC
+ $(function () {
+    $('select').selectric();
+});
+
+
+// FOR SLIDERS
+var mySwiper = new Swiper('.first-slider', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    spaceBetween: 20,
+
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        progressbarOpposite: false,
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
+var swiper = new Swiper('.second-slider', {
+    autoplay: {
+        delay: 100000
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    slidesPerView: 3,
+});
+
+var swiper = new Swiper(".plus-images-slider", {
+    direction: 'horizontal',
+    spaceBetween: 10,
+})
+
+
+var swiper = new Swiper('.second-slider-mobile', {
+    autoplay: {
+        delay: 100000
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    slidesPerView: 1,
+    width: 228,
+});
+
+
+var swiper = new Swiper('.clients-feedback-content', {
+    slidesPerView: 4,
+    width:1000,
+    autoplay: {
+        delay: 3000
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
+
+var swiper = new Swiper('.clients-feedback-mobile', {
+    slidesPerView: 1,
+    width: 300,
+    spaceBetween: 10,
+    autoplay: {
+        delay: 10000
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
+
+var swiper = new Swiper('.recipe-slider', {
+    // autoplay: {
+    //     delay: 3000
+    // },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    slidesPerView: 3,
+});
